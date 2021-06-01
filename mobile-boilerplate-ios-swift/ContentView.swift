39586f4e3be814ccf1f18ea014583b8f6aedcccd
Anton Fenske <anton@devskills.co>
2021-06-01 12:28:37 +0200

    Init boilerplate

---
+++ mobile-boilerplate-ios-swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mobile-boilerplate-ios-swift
+//
+//  Created by Anton Fenske on 2021-06-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Start by editing ContentView.swift")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mobile-boilerplate-ios-swift/mobile_boilerplate_ios_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobile_boilerplate_ios_swiftApp.swift
+//  mobile-boilerplate-ios-swift
+//
+//  Created by Anton Fenske on 2021-06-01.
+//
+
+import SwiftUI
+
+@main
+struct mobile_boilerplate_ios_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
